f45bf4a77257bed935166cc6c03629ebed9178ec
luissilva5002 <luisandremdsilva@gmail.com>
2025-11-02 14:42:41 +0000

    Cleaned duplicate code on state machines and link layer

--- main.c
+++ main.c
@@ -10,11 +10,6 @@
 #define N_TRIES 3
 #define TIMEOUT 4
 
-// Arguments:
-//   $1: /dev/ttySxx
-//   $2: baud rate
-//   $3: tx | rx
-//   $4: filename
 int main(int argc, char *argv[])
 {
     if (argc < 5)
